312ff7c9633198418b16cb174d3992b27669f279
Roman Heinrich <roman.heinrich@gmail.com>
2023-04-13 18:39:15 +0200

    Feat: Explicitly open DBs via URL in check module

--- lib/maxo_sql/check.ex
+++ lib/maxo_sql/check.ex
@@ -2,15 +2,15 @@ defmodule MaxoSql.Check do
   alias MaxoSql.Driver.Mysql
   alias MaxoSql.Driver.Psql
 
-  def mysql do
-    {:ok, pid} = Mysql.start()
-    Mysql.query!(pid, "select 1") |> IO.inspect()
+  def psql do
+    {:ok, pid} = Psql.start("postgresql://postgres:postgres@127.0.0.1:5551/maxo_sql")
+    Psql.query!(pid, "select 1") |> IO.inspect()
     GenServer.stop(pid)
   end
 
-  def psql do
-    {:ok, pid} = Psql.start()
-    Psql.query!(pid, "select 1") |> IO.inspect()
+  def mysql do
+    {:ok, pid} = Mysql.start("mysql://root:mysql@127.0.0.1:5552/maxo_sql")
+    Mysql.query!(pid, "select 1") |> IO.inspect()
     GenServer.stop(pid)
   end
 end
