122b4c861e04780eb7a9b24029c1f246449a74ed
Roman Heinrich <roman.heinrich@gmail.com>
2023-04-13 16:18:47 +0200

    Chore: specify in result which fields belong to which driver

--- lib/maxo_sql/result.ex
+++ lib/maxo_sql/result.ex
@@ -3,12 +3,15 @@ defmodule MaxoSql.Result do
   Common result struct
   """
   @type t :: %__MODULE__{}
-  defstruct command: nil,
-            columns: nil,
+  defstruct columns: nil,
             type: nil,
             rows: nil,
             num_rows: 0,
-            last_insert_id: nil,
             connection_id: nil,
-            messages: []
+            # psql
+            command: nil,
+            messages: [],
+            ## mysql
+            last_insert_id: nil,
+            num_warnings: nil
 end
